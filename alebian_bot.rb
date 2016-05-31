8367920ba5eb0caa34ec171b0bc9c1f599765c8c
alebian <alebezdjian@gmail.com>
2016-05-30 22:59:10 -0300

    Added Alan Perlis quotes

--- alebian_bot.rb
+++ alebian_bot.rb
@@ -1,4 +1,5 @@
 require 'telegram/bot'
+require_relative 'alan_perlis'
 require_relative 'chuck_norris'
 
 class AlebianBot
@@ -8,6 +9,8 @@ class AlebianBot
         case message.text
         when '/chuck'
           bot.api.send_message(chat_id: message.chat.id, text: ChuckNorris.random_quote)
+        when '/perlis'
+          bot.api.send_message(chat_id: message.chat.id, text: AlanPerlis.random_quote)
         end
       end
     end
