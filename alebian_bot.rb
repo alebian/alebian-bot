14471f2eeb13bb67975d88f89c6c04ad204b2bb2
alebian <alebezdjian@gmail.com>
2016-06-14 12:37:34 -0300

    Improved style

--- alebian_bot.rb
+++ alebian_bot.rb
@@ -1,18 +1,33 @@
 require 'telegram/bot'
-require_relative 'alan_perlis'
-require_relative 'chuck_norris'
+require_relative 'lib/alan_perlis'
+require_relative 'lib/chuck_norris'
 
 class AlebianBot
-  def self.run(token)
-    Telegram::Bot::Client.run(token) do |bot|
-      bot.listen do |message|
-        case message.text
-        when '/chuck'
-          bot.api.send_message(chat_id: message.chat.id, text: ChuckNorris.random_quote)
-        when '/perlis'
-          bot.api.send_message(chat_id: message.chat.id, text: AlanPerlis.random_quote)
+  HELP_MESSAGE = 'Available commands: /help, /chuck, /perlis'.freeze
+
+  class << self
+    def run(token)
+      Telegram::Bot::Client.run(token) do |bot|
+        bot.listen do |message|
+          case message.text
+          when '/help'
+            bot.api.send_message message_hash(message, HELP_MESSAGE)
+          when '/chuck'
+            bot.api.send_message message_hash(message, ChuckNorris.random_quote)
+          when '/perlis'
+            bot.api.send_message message_hash(message, AlanPerlis.random_quote)
+          end
         end
       end
+    end
+
+    private
+
+    def message_hash(message, text)
+      {
+        chat_id: message.chat.id,
+        text: text
+      }
     end
   end
 end
